56d393d10669c01d8f1a57a6eba5c34fb9e8f008
Henry <dongyang1993@outlook.com>
2020-05-25 08:54:52 +0800

    eureka

--- cloud-consumer-order/src/main/java/org/springcloud/controller/OrderController.java
+++ cloud-consumer-order/src/main/java/org/springcloud/controller/OrderController.java
@@ -5,10 +5,15 @@ import org.springcloud.entity.CommonResult;
 import org.springcloud.entity.Payment;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Collections;
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -18,6 +23,8 @@ public class OrderController {
 
     @Autowired
     private RestTemplate restTemplate;
+    @Autowired
+    private DiscoveryClient discoveryClient;
 
     @GetMapping("/consumer/payment/create")
     public CommonResult<Payment> create(Payment payment) {
@@ -27,5 +34,12 @@ public class OrderController {
     @GetMapping("/consumer/payment/get/{id}")
     public CommonResult<Payment> getPayment(@PathVariable("id") Long id) {
         return restTemplate.getForObject(PAYMENT_URL+"/payment/get/"+id, CommonResult.class);
+    }
+
+    @GetMapping("/discovery/get")
+    public Object getDiscovery() {
+        List<ServiceInstance> instances = discoveryClient.getInstances("cloud-paymet-service");
+        Collections.shuffle(instances);
+        return instances.get(0).getUri();
     }
 }
