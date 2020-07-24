3dfb0e0c8f32c5b64e334cf21f21b62ada2a029a
Henry <dongyang1993@outlook.com>
2020-07-24 12:25:42 +0800

    gateway

--- cloud-consumer-order/src/main/java/org/springcloud/controller/OrderController.java
+++ cloud-consumer-order/src/main/java/org/springcloud/controller/OrderController.java
@@ -7,25 +7,23 @@ import org.springcloud.service.PaymentFeignService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 @Slf4j
+@RequestMapping("/consumer/payment")
 @RestController
 public class OrderController {
 
     @Autowired
     private PaymentFeignService paymentFeignService;
 
-    @Autowired
-    private RestTemplate restTemplate;
-
-    @GetMapping("/consumer/payment/create")
+    @GetMapping("/create")
     public CommonResult<Integer> create(Payment payment) {
         return paymentFeignService.create(payment);
     }
 
-    @GetMapping("/consumer/payment/get/{id}")
+    @GetMapping("/get/{id}")
     public CommonResult<Payment> getPayment(@PathVariable("id") Long id) {
         return paymentFeignService.getPaymentById(id);
     }
---
+++ cloud-gateway/src/main/java/org/springcloud/config/GateWayConfig.java
@@ -0,0 +1,16 @@
+package org.springcloud.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GateWayConfig {
+
+    @Bean
+    public RouteLocator routeLocator (RouteLocatorBuilder builder) {
+        RouteLocatorBuilder.Builder routes = builder.routes();
+        return routes.build();
+    }
+}
